6890c534c029b421ce925d366a3caafe8cfc1f55
Fabian Bleile <fabian.bleile@posteo.de>
2025-09-02 09:56:32 +0200

    cm4_inter

--- stadion/utils/launch.py
+++ stadion/utils/launch.py
@@ -52,7 +52,7 @@ def generate_run_commands(command_list=None,
         slurm_cmd += '--get-user-env '
         slurm_cmd += '--export=NONE' # '--export=NONE '
         slurm_cmd += '--clusters=serial '
-        slurm_cmd += '--partition=serial_std '
+        slurm_cmd += '--partition=cm4_inter '
         # slurm_cmd += '--qos=cm4_std '
         slurm_cmd += '--nodes=1 '
         slurm_cmd += '--ntasks-per-node=1 '
