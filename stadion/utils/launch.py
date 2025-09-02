9497fe64efccf874b7025775c79e7f67658001f6
Fabian Bleile <fabian.bleile@posteo.de>
2025-09-02 15:30:09 +0200

    no message

--- stadion/utils/launch.py
+++ stadion/utils/launch.py
@@ -52,7 +52,7 @@ def generate_run_commands(command_list=None,
         
         # LRZ cluster serial
         slurm_cmd += '--get-user-env '
-        slurm_cmd += '--export=ALL' # '--export=NONE '
+        slurm_cmd += '--export=ALL ' # '--export=NONE '
         slurm_cmd += '--clusters=serial '
         slurm_cmd += '--partition=serial_std '
         # slurm_cmd += '--qos=cm4_std '
